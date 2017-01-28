a86d2e8d9337db218b79d37118a621d4d91a006d
Jean luc <you@example.com>
2017-01-28 19:29:14 +0100

    synchro

--- source/class/Routing/Request.php
+++ source/class/Routing/Request.php
@@ -6,62 +6,61 @@ class Request implements \Phi\Routing\Interfaces\Request
 {
 
 
+    protected static $mainInstance = null;
 
-	protected static $mainInstance=null;
-
-	protected $isHTTP;
-	protected $uri=null;
-
-
-	protected $protocol;
+    protected $isHTTP;
+    protected $uri = null;
 
 
+    protected $protocol;
 
 
-	public static function getInstance() {
+    public static function getInstance()
+    {
 
-		if(static::$mainInstance===null) {
-			static::$mainInstance=new static();
-		}
-		return static::$mainInstance;
-	}
-
-
-	public function __construct() {
+        if (static::$mainInstance === null) {
+            static::$mainInstance = new static();
+        }
+        return static::$mainInstance;
+    }
 
 
-		if($this->isHTTP()) {
-			$this->URI=$_SERVER['REQUEST_URI'];
-			$this->protocol=$_SERVER['SERVER_PROTOCOL'];
-		}
-	}
+    public function __construct($isHTTP = null)
+    {
 
-	public function getURI() {
-		return $this->URI;
-	}
+        if ($isHTTP === null) {
+            if ($this->isHTTP()) {
+                $this->URI = $_SERVER['REQUEST_URI'];
+                $this->protocol = $_SERVER['SERVER_PROTOCOL'];
+            }
+        } else {
+            $this->isHTTP = $isHTTP;
+        }
+    }
+    
 
-
-
-	public function isHTTP() {
-
-
-		if($this->isHTTP===null) {
-			if(php_sapi_name() == "cli") {
-				$this->isHTTP=false;
-			}
-			else {
-				$this->isHTTP=true;
-			}
-		}
-
-		return $this->isHTTP;
+    public function getURI()
+    {
+        return $this->URI;
+    }
 
 
-	}
+    public function isHTTP()
+    {
 
 
+        if ($this->isHTTP === null) {
+            if (php_sapi_name() == "cli") {
+                $this->isHTTP = false;
+            } else {
+                $this->isHTTP = true;
+            }
+        }
+
+        return $this->isHTTP;
 
 
+    }
 
 
 }
