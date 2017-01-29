7138fc0f5107e925cade7db2e26b01b765b0f854
Jean luc <you@example.com>
2017-01-29 15:58:33 +0100

    synchro

--- source/class/Routing/Router.php
+++ source/class/Routing/Router.php
@@ -5,7 +5,6 @@ namespace Phi\Routing;
 use Phi\HTTP\Header;
 
 
-
 /**
  * Class Router
  * @package Phi
@@ -15,29 +14,31 @@ class Router implements \Phi\Routing\Interfaces\Router
 {
 
 
-    protected $routes=array();
-    protected $headers=array();
+    protected $routes = array();
+    protected $headers = array();
 
 
-    public function addRoute(Route $route, $name) {
-        $this->routes[$name]=$route;
+    public function addRoute(Route $route, $name)
+    {
+        $this->routes[$name] = $route;
         return $route;
     }
 
 
-    public function getRouteByName($name) {
-        if(isset($this->routes[$name])) {
+    public function getRouteByName($name)
+    {
+        if (isset($this->routes[$name])) {
             return $this->routes[$name];
-        }
-        else {
-            throw new Exception('Route with name "'.$name.'" does not exist');
+        } else {
+            throw new Exception('Route with name "' . $name . '" does not exist');
         }
     }
 
 
-    public function get($validator, $callback, $name=null, $headers=array()) {
-        if($name===null) {
-            $name=$validator;
+    public function get($validator, $callback, $name = null, $headers = array())
+    {
+        if ($name === null) {
+            $name = $validator;
         }
 
         return $this->addRoute(
@@ -48,19 +49,22 @@ class Router implements \Phi\Routing\Interfaces\Router
 
 
     //regexp permettant de valider la fin d'une url se termine sois par "/", "?....." ou fin d'url ($)
-    public function getEndRouteRegexp() {
+    public function getEndRouteRegexp()
+    {
         return '(:?/|\?|$)';
     }
 
 
-    protected function getDefaultRequest() {
+    protected function getDefaultRequest()
+    {
         return new Request();
     }
 
-    public function run(\Phi\Interfaces\Request $request=null) {
+    public function run(\Phi\Interfaces\Request $request = null)
+    {
 
-        if($request==null) {
-            $request=$this->getDefaultRequest();
+        if ($request == null) {
+            $request = $this->getDefaultRequest();
         }
 
 
@@ -70,25 +74,26 @@ class Router implements \Phi\Routing\Interfaces\Router
              * @var \Phi\Route $route
              */
 
-            if($route->validate($request)) {
-                if($route->execute()) {
-                    $headers=$route->getHeaders();
-                    foreach ($headers as $name=>$value) {
-                        $this->headers[$name]=new Header($name, $value);
+            if ($route->validate($request)) {
+                if ($route->execute()) {
+                    $headers = $route->getHeaders();
+                    foreach ($headers as $name => $value) {
+                        $this->headers[$name] = new Header($name, $value);
                     }
                     break;
                 }
             }
         }
-        $buffer=ob_get_clean();
-        if($request->isHTTP()) {
+        $buffer = ob_get_clean();
+        if ($request->isHTTP()) {
             $this->sendHeaders();
         }
         echo $buffer;
     }
 
 
-    public function sendHeaders() {
+    public function sendHeaders()
+    {
         foreach ($this->headers as $header) {
             $header->send();
         }
