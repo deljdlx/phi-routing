d0ec8f534d6662c2ff66c6c3cbf688c5bb7b36ea
ElBiniou <elbiniou@gmail.com>
2019-05-12 08:55:11 +0200

    synchro

---
+++ exemple/_autoload.php
@@ -0,0 +1,10 @@
+<?php
+
+
+require(__DIR__.'/../source/autoload.php');
+
+require(__DIR__.'/../../phi-http/source/autoload.php');
+require(__DIR__.'/../../phi-session/source/autoload.php');
+require(__DIR__.'/../../phi-core/source/autoload.php');
+require(__DIR__.'/../../phi-event/source/autoload.php');
+require(__DIR__.'/../../phi-traits/source/autoload.php');
---
+++ source/class/Exception.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Phi\Routing;
+
+class Exception extends \Phi\Core\Exception
+{
+
+
+}
+
